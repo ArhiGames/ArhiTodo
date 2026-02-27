52ae3ea6159b3ca4b268f3ca010cfe2dbfc42916
Arhi Games <arvid.hilgers@gmx.de>
2026-02-27 23:12:19 +0100

    [F/Card] Added "no-priority" as a way to remove priority from a card

--- frontend/src/Components/Card/CardUrgencyLabel.tsx
+++ frontend/src/Components/Card/CardUrgencyLabel.tsx
@@ -6,12 +6,13 @@ interface Props {
 
 const CardUrgencyLabel = ({ cardUrgencyLevel, onClick, ref } : Props) => {
 
-    if (cardUrgencyLevel <= 0 || cardUrgencyLevel >= 5) return null;
-    const cardUrgencyString: string = ["Low", "Medium", "High", "Urgent"][cardUrgencyLevel - 1];
+    if (cardUrgencyLevel < 0 || cardUrgencyLevel >= 5) return null;
+    const cardUrgencyString: string = ["No priority", "Low", "Medium", "High", "Urgent"][cardUrgencyLevel];
 
     return (
         <div ref={ref} onClick={onClick} className={`card-urgency-label ${onClick ? "clickable" : ""}`}>
-            <img className={`card-urgency-imgsvg ${cardUrgencyString.toLowerCase()}`} src={`/urgency-${cardUrgencyString.toLowerCase()}.svg`} alt="" height="18px"/>
+            <img className={`card-urgency-imgsvg ${cardUrgencyString.toLowerCase()}`}
+                 src={`/urgency-${cardUrgencyString.replace(" ", "-").toLowerCase()}.svg`} alt="" height="18px"/>
             <p>{cardUrgencyString}</p>
         </div>
     )
