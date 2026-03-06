da273d809707b8cb883c0af3e74c5c686db5e05b
Arhi Games <arvid.hilgers@gmx.de>
2026-03-06 21:24:32 +0100

    [F/DndFixes] Creating boards now completely works, added to project / removed from / updated

--- frontend/src/Contexts/Kanban/Actions/Implementation/Boards/createBoardAction.ts
+++ frontend/src/Contexts/Kanban/Actions/Implementation/Boards/createBoardAction.ts
@@ -1,7 +1,16 @@
-import type {Board, KanbanState} from "../../../../../Models/States/KanbanState.ts";
+import type {Board, KanbanState, Project} from "../../../../../Models/States/KanbanState.ts";
 import type { CreateBoardPayload } from "../../KanbanAction.ts";
 
 const createBoardAction = (state: KanbanState, payload: CreateBoardPayload): KanbanState => {
+
+    const project: Project | undefined = state.projects.get(payload.projectId);
+    if (!project) return state;
+
+    const newProjects: Map<number, Project> = new Map(state.projects);
+    newProjects.set(payload.projectId, {
+        ...project,
+        boardIds: [...project.boardIds, payload.boardId],
+    });
 
     const newBoards: Map<number, Board> = new Map(state.boards);
     newBoards.set(payload.boardId, {
@@ -16,6 +25,7 @@ const createBoardAction = (state: KanbanState, payload: CreateBoardPayload): Kan
 
     return {
         ...state,
+        projects: newProjects,
         boards: newBoards
     }
 
--- frontend/src/Contexts/Kanban/Actions/Implementation/Boards/createBoardSucceededAction.ts
+++ frontend/src/Contexts/Kanban/Actions/Implementation/Boards/createBoardSucceededAction.ts
@@ -1,12 +1,21 @@
-import type {Board, KanbanState} from "../../../../../Models/States/KanbanState.ts";
+import type {Board, KanbanState, Project} from "../../../../../Models/States/KanbanState.ts";
 import type {CreateBoardSucceededPayload} from "../../KanbanAction.ts";
 
 const createBoardSucceededAction = (state: KanbanState, payload: CreateBoardSucceededPayload) => {
 
-    const newBoards: Map<number, Board> = new Map(state.boards);
-
     const oldBoard: Board | undefined = state.boards.get(payload.predictedBoardId);
     if (!oldBoard) return state;
+
+    const oldProject: Project | undefined = state.projects.get(oldBoard.projectId);
+    if (!oldProject) return state;
+
+    const newProjects: Map<number, Project> = new Map(state.projects);
+    const newBoards: Map<number, Board> = new Map(state.boards);
+
+    newProjects.set(oldProject.projectId, {
+        ...oldProject,
+        boardIds: oldProject.boardIds.map((boardId: number) => boardId === payload.predictedBoardId ? payload.actualBoardId : boardId)
+    })
 
     newBoards.set(payload.actualBoardId, {
         ...oldBoard,
@@ -16,6 +25,7 @@ const createBoardSucceededAction = (state: KanbanState, payload: CreateBoardSucc
 
     return {
         ...state,
+        projects: newProjects,
         boards: newBoards
     }
 
