d431bc58b326d0c56f801b9765159e02a0556918
Arhi Games <arvid.hilgers@gmx.de>
2026-03-12 22:50:20 +0100

    [F/Filtering] You can now filter for additional users + actually view them

--- frontend/src/Components/Board/BoardComp/BoardCompHeaderMembersComp.tsx
+++ frontend/src/Components/Board/BoardComp/BoardCompHeaderMembersComp.tsx
@@ -7,19 +7,29 @@ import {useKanbanState} from "../../../Contexts/Kanban/Hooks.ts";
 import {useAuth} from "../../../Contexts/Authentication/useAuth.ts";
 import {useParams} from "react-router-dom";
 import {useCardsSearch} from "../../../Contexts/Kanban/Cards/SearchCardsContexts.ts";
+import Popover from "../../../lib/Popover/Popover.tsx";
+import GeneralUserViewerComp from "../../User/GeneralUserViewerComp.tsx";
 
 const UserViewerCompFooter = ({ user, close }: { user: PublicUserGetDto, close: () => void }) => {
 
     const cardSearch = useCardsSearch();
 
+    function isFiltered(): boolean {
+        return cardSearch.filteringUserIds.includes(user.userId);
+    }
+
     function onViewUserClick() {
-        cardSearch.setFilteringUserIds((prev: string[]) => [...prev, user.userId]);
-        close();
+        if (isFiltered()) {
+            cardSearch.setFilteringUserIds((prev: string[]) => prev.filter((userId: string) => userId !== user.userId));
+        } else {
+            cardSearch.setFilteringUserIds((prev: string[]) => [...prev, user.userId]);
+            close();
+        }
     }
 
     return (
         <div className="user-viewer-filter-footer-div">
-            <button className="button standard-button" onClick={onViewUserClick}>Show cards with user</button>
+            <button className="button standard-button" onClick={onViewUserClick}>{isFiltered() ? "Remove filter" : "Show cards with user"}</button>
         </div>
     )
 
@@ -34,6 +44,8 @@ const BoardCompHeaderMembersComp = () => {
     const searchCards = useCardsSearch();
 
     const boardMembersButtonRef = useRef<HTMLImageElement>(null);
+    const viewRemainingUsersDiv = useRef<HTMLDivElement>(null);
+    const [isViewingRemainingMembers, setIsViewingRemainingMembers] = useState<boolean>(false);
     const [isEditingMembers, setIsEditingMembers] = useState<boolean>(false);
 
     function onUserIconPressed(user: PublicUserGetDto) {
@@ -44,8 +56,7 @@ const BoardCompHeaderMembersComp = () => {
         return !searchCards.filteringUserIds.includes(user.userId);
     }
 
-    function getMembersJsx() {
-
+    function getMembers(): PublicUserGetDto[] {
         const project: Project | undefined = kanbanState.projects.get(Number(projectId));
         const board: Board | undefined = kanbanState.boards.get(Number(boardId));
         if (!project || !board) return [];
@@ -64,35 +75,59 @@ const BoardCompHeaderMembersComp = () => {
             if (b.userId === appUser?.id) return 1;
             return 0;
         });
+        return members;
+    }
 
-        const remainingBoardMembers: number = members.length - 6;
+    function getMembersJsx() {
+        const members: PublicUserGetDto[] = getMembers();
+        const remainingBoardMembers: number = members.length - 5;
         return (
             <div className="board-members">
-                {members.slice(0, 6).map((boardMember: PublicUserGetDto) => {
-                    return <CardUserIcon key={boardMember.userId} canViewDetails={canViewDetails} size="medium" user={boardMember}
-                                         onClick={() => onUserIconPressed(boardMember)}
-                                         footer={UserViewerCompFooter} selected={searchCards.filteringUserIds.includes(boardMember.userId)}/>;
+                {members.map((boardMember: PublicUserGetDto, index: number) => {
+                    if (index >= 5 && !searchCards.filteringUserIds.includes(boardMember.userId)) return null;
+                    return (
+                        <CardUserIcon key={boardMember.userId} canViewDetails={canViewDetails} size="medium" user={boardMember}
+                                      onClick={() => onUserIconPressed(boardMember)}
+                                      footer={UserViewerCompFooter} selected={searchCards.filteringUserIds.includes(boardMember.userId)}/>
+                    );
                 })}
-                { remainingBoardMembers > 0 && <div style={{ opacity: ".55" }} className="card-member-card medium">+{remainingBoardMembers}</div> }
+                { remainingBoardMembers > 0 && <div style={{ opacity: ".55" }} onClick={() => setIsViewingRemainingMembers(prev => !prev)}
+                                                    className="card-member-card medium" ref={viewRemainingUsersDiv}>+{remainingBoardMembers}</div> }
             </div>
         )
-
     }
 
     return (
-        <section>
-            { permissions.hasManageBoardUsersPermission() && (
-                <>
-                    <img src="/settings-icon.svg" alt="Settings" style={{ height: "36px", marginRight: "0.25rem" }}
-                         ref={boardMembersButtonRef} onClick={() => setIsEditingMembers((prev: boolean) => !prev)}
-                         className="icon clickable"/>
-                    { isEditingMembers && (
-                        <BoardUserSelector element={boardMembersButtonRef} close={() => setIsEditingMembers(false)}/>
-                    )}
-                </>
+        <>
+            <section>
+                { permissions.hasManageBoardUsersPermission() && (
+                    <>
+                        <img src="/settings-icon.svg" alt="Settings" style={{ height: "36px", marginRight: "0.25rem" }}
+                             ref={boardMembersButtonRef} onClick={() => setIsEditingMembers((prev: boolean) => !prev)}
+                             className="icon clickable"/>
+                        { isEditingMembers && (
+                            <BoardUserSelector element={boardMembersButtonRef} close={() => setIsEditingMembers(false)}/>
+                        )}
+                    </>
+                )}
+                { getMembersJsx() }
+            </section>
+            { isViewingRemainingMembers && (
+                <Popover close={() => setIsViewingRemainingMembers(false)} element={viewRemainingUsersDiv} triggerElement={viewRemainingUsersDiv}>
+                    <div className="remaining-members-filter-popover-div">
+                        {getMembers().slice(5).map((member: PublicUserGetDto) => {
+                            if (searchCards.filteringUserIds.includes(member.userId)) return null;
+                            return (
+                                <div className="remaining-member-filter-comp">
+                                    <GeneralUserViewerComp user={member} options={{ showProjectOwner: true, showBoardOwner: true }}
+                                                           close={() => setIsViewingRemainingMembers(false)} footer={UserViewerCompFooter}/>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </Popover>
             )}
-            { getMembersJsx() }
-        </section>
+        </>
     )
 
 }
